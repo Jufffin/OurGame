f9e8fe69c3da0ae57147d04da19f108a718e41ce
galean <Orean@ukr.net>
2012-07-14 10:21:45 +0300

    New Project
    proj

---
+++ OurGame/OurGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OurGame
+//
+//  Created by Андрей Плотников on 7/14/12.
+//  Copyright (c) 2012 Андрей Плотников. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OurGame/OurGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OurGame
+//
+//  Created by Андрей Плотников on 7/14/12.
+//  Copyright (c) 2012 Андрей Плотников. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
